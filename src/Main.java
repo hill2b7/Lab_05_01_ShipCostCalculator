cf894c7914d06b2aa5b5f6e0e64c5a826233285d
blake <hill2b7@mail.uc.edu>
2023-02-07 17:51:03 -0500

    init Project

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args)
     {
-        double priceOfItem = 101.00;
-        double totalPrice = 0;
-        double shippingCost = 0;
-        final double SHIPPING_COST_FACTOR = 100.00;
-        final double TAX_PERCENTAGE = .02;
-        if (priceOfItem > SHIPPING_COST_FACTOR)
+        double priceOfItem = 101.00; //declarations
+        double totalPrice = 0; //declarations
+        double shippingCost = 0; //declarations
+        final double SHIPPING_COST_FACTOR = 100.00; //using final because it is a constant that doesn't change
+        final double TAX_PERCENTAGE = .02; //using final because it is a constant that doesn't change
+        if (priceOfItem > SHIPPING_COST_FACTOR) //If the price of the item is greater than the cost factor then code in the block is executed
         {
             System.out.println("Shipping is free! Your total cost is " + priceOfItem);
         }
-        else if (priceOfItem < SHIPPING_COST_FACTOR)
+        else if (priceOfItem < SHIPPING_COST_FACTOR) // if the first if statement is not true then this condition would be next. If true then the code block runs
         {
-            shippingCost = priceOfItem * TAX_PERCENTAGE;
-            totalPrice = shippingCost + priceOfItem;
-            System.out.println("Your shipping cost is " + shippingCost + " and your total cost is " + totalPrice);
+            shippingCost = priceOfItem * TAX_PERCENTAGE; //calculation for shipping cost
+            totalPrice = shippingCost + priceOfItem; // calculation for total price
+            System.out.println("Your shipping cost is " + shippingCost + " and your total cost is " + totalPrice); //system output for results
         }
     }
 }
